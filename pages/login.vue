8a1f3bbbcd5d7c79b1981455e9250ce86c6876be
jae lee <jaequery@gmail.com>
2020-09-24 01:31:02 -0700

    - updated logging in

--- pages/login.vue
+++ pages/login.vue
@@ -79,7 +79,9 @@
         <div class="mt-6">
           <button
             type="submit"
-            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+            class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
+            :disabled="isLoggingIn"
+            :class="[ isLoggingIn ? 'bg-indigo-500': 'bg-indigo-600']"
           >
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <svg
